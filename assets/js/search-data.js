6eed36743f5afbfcdbd989109afd3ab9835c14aa
oskuddar <o.kuddar@gmail.com>
2026-03-15 23:59:11 +0000

    Deploying to gh-pages from @ oskuddar/oskuddar.github.io@ba9c683e9ae74bc40b7fbc82d1ac5c66374ba2ca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-started-my-ph-d-in-bioinformatics-at-callahan-lab-at-north-carolina-state-university",
+          title: 'I started my Ph.D. in Bioinformatics at Callahan Lab at North Carolina State...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
