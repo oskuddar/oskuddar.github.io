c1f18bacfca0692595cb05fec7737c8636964826
oskuddar <o.kuddar@gmail.com>
2026-03-16 22:03:16 +0000

    Deploying to gh-pages from @ oskuddar/oskuddar.github.io@8334942f8913f5dc16df2166e4e10b92a3ea61e9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,13 +51,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
@@ -554,14 +547,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6F.%6B%75%64%64%61%72@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
         title: 'Inspire HEP',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://inspirehep.net/authors/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -582,7 +575,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://soundcloud.com/versatilefractalism", "_blank");
         },
       },{
       id: 'light-theme',
