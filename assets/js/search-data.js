d5f07193c49f789c8f21b9f8b043ae5646d08720
oskuddar <o.kuddar@gmail.com>
2026-03-16 00:23:17 +0000

    Deploying to gh-pages from @ oskuddar/oskuddar.github.io@efe29cb4b95bdd1f948a4acb1841e12619cad58d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -445,14 +445,14 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2_/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-i-started-my-ph-d-in-bioinformatics-at-callahan-lab-at-north-carolina-state-university",
+            },},{id: "news-i-started-my-ph-d-in-bioinformatics-at-callahan-lab-at-north-carolina-state-university",
           title: 'I started my Ph.D. in Bioinformatics at Callahan Lab at North Carolina State...',
           description: "",
           section: "News",},{id: "news-presented-my-poster-titled-trnl-database-generation-and-curation-at-the-genomic-sciences-graduate-student-association",
           title: 'Presented my poster titled “trnL Database Generation and Curation” at the Genomic Sciences...',
+          description: "",
+          section: "News",},{id: "news-won-the-symposium-presentation-award-for-a-15-minute-talk-titled-validating-dna-metabarcoding-for-characterization-of-pollen-from-surface-soils-at-the-genomic-sciences-symposium-ncsu",
+          title: 'Won the Symposium Presentation Award for a 15-minute talk titled “Validating DNA Metabarcoding...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
