911a695d627346490778ee094009f9e42cfc7c92
oskuddar <o.kuddar@gmail.com>
2026-03-17 01:44:15 +0000

    Deploying to gh-pages from @ oskuddar/oskuddar.github.io@791035cf0ab1d733fc9e34701b09eb71f301db0b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-roles",
-          title: "Roles",
+          title: "roles",
           description: "",
           section: "Navigation",
           handler: () => {
