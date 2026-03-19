76e8baef7e0d136fa91f339d6e565a4d2c39cbe0
oskuddar <o.kuddar@gmail.com>
2026-03-19 21:13:33 +0000

    Deploying to gh-pages from @ oskuddar/oskuddar.github.io@874b37f7a535b1ea97ca4550b6ac0372f177ff51 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "Please click the PDF icon to see the CV.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
